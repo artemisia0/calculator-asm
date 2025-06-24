6c984eda363f0e18d44ed5eeaf750701bd30bdd4
Artem <artemisia8192@gmail.com>
2025-06-24 12:29:50 +0200

    Added minimalistic error handling logic (nan check)

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <string.h>
+#include <math.h>
 
 
 // Implemented in asm module and then linked together
@@ -19,12 +20,14 @@ int main() {
     if (fgets(input_line, INPUT_LINE_SIZE, stdin) == NULL) {
       break;
     }
+    double res = eval_string_expression(input_line, strlen(input_line));
+    if (isnan(res)) {
+      printf("\033[1;31mInvalid expression error :(\033[0;m\n");
+      break;
+    }
     // I also added awesome style, colors + bold font
     // It should work fine on most linux
-    printf("\033[1;32m%.15lf\033[0;m\n",
-       eval_string_expression(input_line, strlen(input_line)));
-    // If colors are not supported then try this
-    // printf("%f\n", eval_string_expression(input_line, strlen(input_line)));
+    printf("\033[1;32m%.15lf\033[0;m\n", res);
   }
   return 0;
 }
