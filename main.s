57c80af9ca48191f82f3df0f62acc585698cbad5
Artem <artemisia8192@gmail.com>
2025-06-24 17:14:04 +0200

    It now supports something like 2+2 but does not support 2+2*2

--- main.s
+++ main.s
@@ -2,6 +2,9 @@
 nan_value: .double 0.0
 zero: .double 0.0
 input_string: .quad 0
+input_string_size: .quad 0
+sign_mask: .quad 0x8000000000000000   # sign bit for IEEE 754 double
+strtod_end: .quad 0
 
 .text
 .globl eval_string_expression
@@ -10,38 +13,26 @@ eval_string_expression:  # Return value is in %xmm0
   push %rbp
   mov %rsp, %rbp
 
-  # Clear all xmm registers at program start
-  # I know it looks very strange, I actually need xmm0 to xmm4 only
+  # Clear some xmm registers that will be used
+  # because right 8 bytes will be used only but registers are 16 bytes size.
+  # It probably is not necessary but highly recommended anyway
   pxor %xmm0, %xmm0
   pxor %xmm1, %xmm1
   pxor %xmm2, %xmm2
   pxor %xmm3, %xmm3
   pxor %xmm4, %xmm4
-  pxor %xmm5, %xmm5
-  pxor %xmm6, %xmm6
-  pxor %xmm7, %xmm7
-  pxor %xmm8, %xmm8
-  pxor %xmm9, %xmm9
-  pxor %xmm10, %xmm10
-  pxor %xmm11, %xmm11
-  pxor %xmm12, %xmm12
-  pxor %xmm13, %xmm13
-  pxor %xmm14, %xmm14
-  pxor %xmm15, %xmm15
 
   # Calculating nan_value by dividing zero by zero
   movsd zero(%rip), %xmm0
   divsd zero(%rip), %xmm0
   movsd %xmm0, nan_value(%rip)
 
-  # I want to store the pointer to the input string at input_string(%rip)
-  sub $16, %rsp
   mov %rdi, input_string(%rip)
+  mov %rsi, input_string_size(%rip)
   xor %rcx, %rcx  # Input string index is in rcx
 
   call process_sum
 
-  add $16, %rsp
   leave
   ret
 
@@ -119,25 +110,102 @@ process_product_loop_exit:
   leave
   ret
 
-// TODO FIXME
 process_term:
   push %rbp
   mov %rsp, %rbp
 
-  movsd zero(%rip), %xmm0
+  # Testing if there is a number on input
+  call peek_op
+  mov %eax, %edi
+  push %rcx  # two times for stack alignment
+  push %rcx
+  call isdigit
+  pop %rcx
+  pop %rcx
+  cmp $0, %eax
+  jne process_term_number
+  jmp process_term_exit
 
+  # Testing if there is unary minus on input
+  call peek_op
+  cmpb $'-', %al
+  je process_term_unary_minus
+  jmp process_term_exit
+
+  # Testing if there is left parentesis on input
+  call peek_op
+  cmpb $'(', %al
+  je process_term_grouping
+  jmp process_term_exit
+
+process_term_exit:
+
+  leave
+  ret
+
+# double strtod(const char* str, char** end);
+process_term_number:
+  mov input_string(%rip), %rdi
+  add %rcx, %rdi
+  lea strtod_end(%rip), %rsi
+  push %rcx  # pushing twice for stack alignment
+  push %rcx  # but it would probably be simpler to subtract 16 from %rsp
+  call strtod
+  pop %rcx
+  pop %rcx
+  mov strtod_end(%rip), %r8
+  mov input_string(%rip), %rax
+  sub %rax, %r8
+  add %r8, %rcx
+  # parsed number is already in %xmm0 due to strtod function call
+  # most of the work done in this label is just for %rcx index shifting
+  jmp process_term_exit
+
+process_term_unary_minus:
+  inc %rcx  # skipping that minus
+  call process_term
+  movq sign_mask(%rip), %xmm1
+  xorpd %xmm1, %xmm0  # flipping the sign bit
+  jmp process_term_exit
+
+process_term_grouping:
+  inc %rcx  # skipping that '('
+  call process_sum
+  # assert that next char is ')'
+  # for now I assume that the given expression is valid FIXME TODO
+  inc %rcx  # skipping that ')'
+  jmp process_term_exit
+
+skip_whitespaces:
+  push %rbp
+  mov %rsp, %rbp
+skip_whitespaces_loop:
+  call peek_op
+  cmpb $' ', %al
+  jne skip_whitespaces_loop_exit
+  inc %rcx
+  cmp input_string_size(%rip), %rcx
+  jge skip_whitespaces_loop_exit
+  jmp skip_whitespaces_loop
+skip_whitespaces_loop_exit:
   leave
   ret
 
 peek_op:
   push %rbp
   mov %rsp, %rbp
+  cmp input_string_size(%rip), %rcx
+  jge peek_op_bad_index
   mov input_string(%rip), %rsi
   movzbl (%rsi, %rcx, 1), %eax
+peek_op_exit:
   leave
   ret
+peek_op_bad_index:
+  mov $0, %eax
+  jmp peek_op_exit
 
-apply_sum_op:  # %xmm1 ?= %xmm0  that is return value is in %xmm1, NOT %xmm0
+apply_sum_op:  # %xmm1 ?= %xmm0 (return value is in %xmm1, NOT %xmm0)
   push %rbp
   mov %rsp, %rbp
 
