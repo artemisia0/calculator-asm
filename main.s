9a67d2f1a5b92cc184f28bb53694d62c7a28e5d2
Artem <artemisia8192@gmail.com>
2025-06-27 20:59:59 +0200

    eval_sum function heavy refactor (previously names process_sum) + global variables refactor in .data section

--- main.s
+++ main.s
@@ -1,35 +1,34 @@
 .data
-nan_value: .double 0.0
-zero: .double 0.0
-input_string: .quad 0
-input_string_size: .quad 0
-sign_mask: .quad 0x8000000000000000   # sign bit for IEEE 754 double
-strtod_end: .quad 0
+sign_mask:          .quad 0x8000000000000000   # sign bit for IEEE 754 double
+nan_value:          .quad 0x7ff8000000000000
+strtod_end:         .quad 0  # will be passed as an arg to strtod function
+input_string:       .quad 0
+input_string_size:  .quad 0
+current_ch_index:   .quad 0  # index of a char that is being processed now
 
 .text
 .globl eval_string_expression
 
-eval_string_expression:  # Return value is in %xmm0
+
+# Arg #1: pointer to input string
+# Arg #2: input string size
+# Returns double (in %xmm0)
+eval_string_expression:
   push %rbp
   mov %rsp, %rbp
 
-  # Calculating nan_value by dividing zero by zero
-  movsd zero(%rip), %xmm0
-  divsd zero(%rip), %xmm0
-  movsd %xmm0, nan_value(%rip)
-
   mov %rdi, input_string(%rip)
   mov %rsi, input_string_size(%rip)
-  xor %rcx, %rcx  # Input string index is in rcx
 
   # Clear some xmm registers that will be used
   pxor %xmm0, %xmm0
-  pxor %xmm1, %xmm1
-  pxor %xmm2, %xmm2
+  pxor %xmm8, %xmm8
+  pxor %xmm9, %xmm9
 
-  call process_sum
+  call eval_sum
 
-  cmp input_string_size(%rip), %rcx
+  mov current_ch_index(%rip), %rax
+  cmp input_string_size(%rip), %rax
   jge eval_string_expression_exit
   movsd nan_value(%rip), %xmm0
 
@@ -37,70 +36,59 @@ eval_string_expression_exit:
   leave
   ret
 
-return_nan:  # helper label for some functions (useful for error handling)
+
+# Returns double (in %xmm0).
+# Note: %xmm8 will be used as a local variable (it is that 'res' from readme)
+eval_sum:
+  push %rbp
+  mov %rsp, %rbp
+  call eval_product
+  movsd %xmm0, %xmm8
+  mov current_ch_index(%rip), %rax
+  cmp input_string_size(%rip), %rax
+  jge eval_sum_exit
+eval_sum_loop:
+# checking if current character is '+' or '-' and if yes
+# then calculating other operand(s) and applying corresponding operation (-/+)
+  mov $'+', %rdi
+  call match_char
+  test %rax, %rax
+  jnz eval_sum_apply_plus_operator
+  mov $'-', %rdi
+  call match_char
+  test %rax, %rax
+  jnz eval_sum_apply_minus_operator
+eval_sum_loop_exit_check:
+# if peek_char() == 0 || peek_char() == ')' then exit from loop
+  call peek_char
+  test %rax, %rax
+  jz eval_sum_exit
+  cmpb $')', %al
+  jz eval_sum_exit
+  jmp eval_sum_exit_with_error  # invalid operator
+eval_sum_apply_plus_operator:
+  mov current_ch_index(%rip), %rax
+  cmp input_string_size(%rip), %rax
+  jge eval_sum_exit_with_error  # there is an operator but no operand
+  call eval_product
+  addsd %xmm0, %xmm8
+  jmp eval_sum_loop
+eval_sum_apply_minus_operator:
+  mov current_ch_index(%rip), %rax
+  cmp input_string_size(%rip), %rax
+  jge eval_sum_exit_with_error  # there is an operator but no operand
+  call eval_product
+  subsd %xmm0, %xmm8
+  jmp eval_sum_loop
+eval_sum_exit:
+  movsd %xmm8, %xmm0  # make that local variable a return value
+  leave
+  ret
+eval_sum_exit_with_error:
   movsd nan_value(%rip), %xmm0
   leave
   ret
 
-process_sum:
-  push %rbp
-  mov %rsp, %rbp
-
-  call process_product  # and so now processed product is in %xmm0
-  movsd %xmm0, %xmm1  # see algorithm in readme, that v is %xmm1, u is %xmm0
-  # if process_product returned NaN then the result will also be NaN
-
-  cmp input_string_size(%rip), %rcx
-  jge process_sum_loop_exit
-
-  call peek_op
-  cmpb $'+', %al
-  je process_sum_loop
-  cmpb $'-', %al
-  je process_sum_loop
-  cmpb $0, %al
-  je process_sum_loop_exit
-  cmpb $')', %al
-  je process_sum_loop_exit
-  jmp return_nan  # got bad infix operator
-
-process_sum_loop:
-  inc %rcx  # consuming operator because we have already processed it
-
-  cmp input_string_size(%rip), %rcx  # error: no right operand
-  jge return_nan
-
-  push %rax  # twice for stack alignment
-  push %rax
-  sub $16, %rsp
-  movaps %xmm1, (%rsp)
-
-  call process_product
-  # if this returns NaN then the result will also be NaN
-
-  movaps (%rsp), %xmm1
-  add $16, %rsp
-  pop %rax
-  pop %rax
-
-  call apply_sum_op
-
-  call peek_op
-  cmpb $'+', %al
-  je process_sum_loop
-  cmpb $'-', %al
-  je process_sum_loop
-  cmpb $0, %al
-  je process_sum_loop_exit
-  cmpb $')', %al
-  je process_sum_loop_exit
-  jmp return_nan
-
-process_sum_loop_exit:
-  movsd %xmm1, %xmm0  # make v return value
-
-  leave
-  ret
 
 process_product:
   push %rbp
